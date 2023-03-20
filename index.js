aa49a4f09d7c72b9b7c4f328d69b0482f6e02027
cliff <cliffmudigo5@gmail.com>
2023-03-20 14:30:12 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// code your solution here
+
+  function superbowlWin(record) {
+    const winningGame = record.find(game => game.team === "Denver Broncos" && game.result === "W");
+    return winningGame ? winningGame.year.toString() : undefined;
+  }
